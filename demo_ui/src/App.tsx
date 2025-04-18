b59c665ed140f45458ef1cfdb837f7e966856145
pirofestionalDev <r4roggit1@gmail.com>
2025-04-18 22:21:12 +0530

    UI files for Thursday

---
+++ demo_ui/src/App.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function PolkaDot() {
+  return (
+    <div className="w-screen h-screen bg-black flex items-center justify-center">
+      <div className="w-16 h-16 bg-white rounded-full shadow-lg hover:bg-blue-600 hover:scale-110 cursor-pointer transition-all duration-200 ease-in-out" />
+    </div>
+  );
+}
---
+++ ui/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <h1>Hello World</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
